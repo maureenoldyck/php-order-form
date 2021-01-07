b819986953d289b60fdc44f87e13d36d54110d45
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-07 10:04:51 +0100

    Add base files

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file is your starting point (= since it's the index)
+// It will contain most of the logic, to prevent making a messy mix in the html
+
+// This line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+// We are going to use session variables so we need to enable sessions
+session_start();
+
+// Use this function when you need to need an overview of these variables
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Your favourite drink', 'price' => 2.5],
+];
+
+$totalValue = 0;
+
+require 'form-view.php';
