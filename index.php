0f08ce755dd62f0007e1a3f3efe863f269b8a9c7
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-07 10:47:27 +0100

    Add products array

--- index.php
+++ index.php
@@ -23,8 +23,32 @@ function whatIsHappening() {
 
 // TODO: provide some products (you may overwrite the example)
 $products = [
-    ['name' => 'Your favourite drink', 'price' => 2.5],
+    ['name' => 'Healthy Mountain Air', 'price' => 25],
+    ['name' => 'Healthy Forest Air', 'price' => 20],
+    ['name' => 'Healthy Ocean Air', 'price' => 15],
+    ['name' => 'Mysterious Air', 'price' => 22.5],
+    ['name' => 'The Air after it Rained', 'price' => 8.5],
+    ['name' => 'The Air of a Sunny Day', 'price' => 20],
+    ['name' => 'Air from the North Sea', 'price' => 5.5],
+    ['name' => 'Air from the Bahamas', 'price' => 50],
+    ['name' => 'Air from Ninove', 'price' => 2.5],
+    ['name' => 'Smoke', 'price' => 2.5],
+]; 
+
+/*
+$products = [
+    ['name' => 'Boyfriend Pillow - XL', 'price' => 25],
+    ['name' => 'Boyfriend Pillow - M', 'price' => 20],
+    ['name' => 'Boyfriend Pillow - S', 'price' => 15],
+    ['name' => 'Girlfriend Pillow - XL', 'price' => 25],
+    ['name' => 'Girlfriend Pillow - M', 'price' => 20],
+    ['name' => 'Girlfriend Pillow - S', 'price' => 15],
+    ['name' => 'Genderqueer Pillow - XL', 'price' => 25],
+    ['name' => 'Genderqueer Pillow - M', 'price' => 20],
+    ['name' => 'Genderqueer Pillow - S', 'price' => 15],
+    ['name' => 'Big Banana Pillow - XL', 'price' => 25],
 ];
+*/
 
 $totalValue = 0;
 
